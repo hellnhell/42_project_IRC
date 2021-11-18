8c7791471733d21ab9e652f330e085199ae57194
NestorAzurmendi <nazurmen@DESKTOP-US35PNP.localdomain>
2021-11-18 18:17:13 +0100

    minifix

--- parseo.cpp
+++ parseo.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parseo.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emartin- <emartin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nazurmen <nazurmen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 13:16:16 by emartin-          #+#    #+#             */
-/*   Updated: 2021/11/18 13:32:59 by emartin-         ###   ########.fr       */
+/*   Updated: 2021/11/18 18:14:29 by nazurmen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void parse_message(std::string buffer)
     std::vector<std::string>    tok_tmp;
     std::vector<std::string>    tokens;
     size_t                      pos;
-	std::string                 tmps;    
+	std::string                 tmps;
     std::stringstream           s(buffer);
     std::istringstream          ss;
-   
+
     if (buffer.empty())
         tokens.push_back("");
     if (((pos = buffer.find('\n')) != std::string::npos) || ((pos = buffer.find('\r')) != std::string::npos))
@@ -31,7 +31,7 @@ void parse_message(std::string buffer)
         ss.str(tok_tmp[1]);
         while(ss >> tmps)
             tokens.push_back(tmps);
-        tokens.erase(tokens.begin());
+//          tokens.erase(tokens.begin()); //N: creo que asin
         tokens.push_back(tok_tmp[2]);
     }
     else
@@ -41,14 +41,23 @@ void parse_message(std::string buffer)
             tokens.push_back(tmps);
         tokens.push_back(tok_tmp[1]);
     }
+
+
+    //N: esto es cutrez para verlo, pero deberia returnear vector string(?)
+   for(auto i: tokens)
+    std::cout << i << std::endl;
+
+
+
     return;
 }
 
 int main()
 {
-    std::string buffer ="cmmd param param : hola que tal";
+    std::string buffer =":prefijo cmmd param param : hola que tal";
     // std::string buffer2 =":prf cmmd param param : asd";
     parse_message(buffer);
     // parse_message(buffer2);
+
     return 0;
 }
